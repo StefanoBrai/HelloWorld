08e8a59c77ccf814c5fafc05a1caa0b532e216f1
StefanoBrai <57347310+StefanoBrai@users.noreply.github.com>
2019-11-06 14:29:01 +0100

    new commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -41,6 +41,19 @@ namespace HelloWorld
 
             //Console.WriteLine(andrea.CalculateBMI());
             Console.WriteLine(andrea.Anagrafica());
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+            ///Array
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+
+            //Metodi equivalenti per creare un array
+            int[] arrayDiInteri = new int[4];
+            int[] arrayDiInteri2 = new int[4] { 1, 2, 3, 4 };      //Ha un'informazione ridondante spazio 4 detto 2 volte
+            int[] arrayDiInteri3 = new int[] { 1, 2, 3, 4 };
+            int[] arrayDiInteri4 = { 1, 2, 3, 4 };
+
+            Person p1 = new Person("Luca","Filippi");
+            
         }
     }
 }
